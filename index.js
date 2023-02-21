292864fbbb9878713b66476959fd356f8f7989e7
dominick <vannusdominick@gmail.com>
2023-02-21 15:40:30 -0800

    allTestsPassing

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return (`This Saturday, I want to ${activity}!`);
+}
+saturdayFun();
+saturdayFun("bathe my dog");
+
+
+const mondayWork = function (activity = 'go to the office') {
+    return (`This Monday, I will ${activity}.`);
+}
+mondayWork();
+
+
+const wrapAdjective = function (flair = '*') {
+    return function (singleParameter = "special") {
+        return `You are ${flair}${singleParameter}${flair}!`;
+    }
+    
+}
+wrapAdjective();
